d50cb4805a563d04ee0b1126cf9f8a32170fc11e
eschoen <erik.schoen@gmail.com>
2018-05-10 14:52:25 -0800

    Added code to calculate recruits for the "core age classes" only, i.e., the 3 most common age classes, 1.2, 1.3, and 1.4.  This expands the dataset to include more year-population combinations, at the expense of discarding information on returns of age 1.1 and 1.5 Chinook (roughly 5% of returns on average).

--- R/SpawnersRecruits.R
+++ R/SpawnersRecruits.R
@@ -5,8 +5,8 @@
 
 # Cook Inlet Chinook project
 
-# Estimate the numbers of spawners by return year and an index of recruits 
-# by brood year, using 2 methods
+# Estimate the numbers of spawners and an index of recruits by brood year, 
+# using 2 methods
 
 # Method 1: Use escapement data only
 # Spawners1 = Escapement Index (aerial counts not scaled up) 
@@ -15,7 +15,7 @@
 # Method 2: Use escapement + terminal harvest data
 # Spawners2 = Estimated Escapement (aerial counts scaled up)
 # Recruits2 = Estimated Escapement (aerial counts scaled up) + Weir Removals 
-#             + Terminal Harvest (below enumeration site)
+#             + Terminal Harvest
 # 3 Estimates for each variable, based on nominal, minimum, and maximum estimates of
 # the rate at which spawners are counted in aerial surveys
 
@@ -52,8 +52,8 @@ aerialExp.max <- 0.6
 
 # Spawners-----------
 spawnersByRY <- escapeFull %>%
-  select(Population, ReturnYear, EscapeIndex = Escapement, WeirRemovals, 
-         EscapementMethod) %>%
+  select(Population, ReturnYear, EscapeIndex = Escapement, EscapementMethod,
+         WeirRemovals) %>%
   filter(ReturnYear > 1979) %>%
   # For aerial survey data, expand Spawners2 using the min, nominal, and max
   # estimates of what fraction of spawners are counted
@@ -74,7 +74,6 @@ spawnersByRY <- escapeFull %>%
 recruitsByRYxBY <- spawnersByRY %>%
   left_join(sport, by = c("Population", "ReturnYear" = "Year")) %>%
   left_join(kenai, by = c("Population", "ReturnYear" = "Year")) %>%
-  # filter(!is.na(Escapement)) %>%
   # Make 5 rows for each population X return year (1 for each age)
   mutate(Age1.1 = NA, Age1.2 = NA, Age1.3 = NA, Age1.4 = NA, Age1.5 = NA) %>%
   gather(key = "Age", value = "Prop", Age1.1:Age1.5) %>%
@@ -115,15 +114,16 @@ recruitsByRYxBY <- spawnersByRY %>%
            SportHarvest * PropEst)
   )
 
+# Calculate spawners and recruits from the 5 major age classes (1.1, 1.2, 1.3, 1.4, 1.5)
 # Calculate Spawners1, Spawners2 (min, nominal, and max),
 # weir removals, and terminal harvest by brood year.  
 recruitsByBrood <- recruitsByRYxBY %>%
   filter(!is.na(Spawners1)) %>%
   group_by(Population, BroodYear) %>%
-  summarize(Spawners1 = sum(Spawners1ByRYxBY, na.rm = T),
-            Spawners2.min = sum(Spawners2.minByRYxBY, na.rm = T),
-            Spawners2.nom = sum(Spawners2.nomByRYxBY, na.rm = T),
-            Spawners2.max = sum(Spawners2.maxByRYxBY, na.rm = T),
+  summarize(Spawners1 = sum(Spawners1ByRYxBY),
+            Spawners2.min = sum(Spawners2.minByRYxBY),
+            Spawners2.nom = sum(Spawners2.nomByRYxBY),
+            Spawners2.max = sum(Spawners2.maxByRYxBY),
             WeirRemovals = sum(WeirRemovalsByRYxBY, na.rm = T),
             TermHarvest = sum(TermHarvestByRYxBY, na.rm = T),
             # Use these values to calculate Recruits1 and Recruits2 
@@ -132,48 +132,64 @@ recruitsByBrood <- recruitsByRYxBY %>%
             Recruits2.min = round(Spawners2.min + WeirRemovals + TermHarvest),
             Recruits2.nom = round(Spawners2.nom + WeirRemovals + TermHarvest),
             Recruits2.max = round(Spawners2.max + WeirRemovals + TermHarvest),
-            # Determine how many age classes with data are included in each brood year
-            NumAgeClasses = sum(!is.na(Spawners1ByRYxBY)),
-            Top5AgeClasses = NumAgeClasses == 5,
-            NumCoreAgeClasses = sum(CoreAgeClass),
-            Top3AgeClasses = NumCoreAgeClasses == 3)  %>%
-  select(Population, BroodYear, Recruits1:Top3AgeClasses)
+            # Remove recruit estimates for brood years lacking data from any of the
+            # 5 major age classes
+            NumAgeClasses = sum(!is.na(Spawners1ByRYxBY)))  %>%
+  filter(NumAgeClasses == 5) %>%
+  select(Population, BroodYear, Recruits1:NumAgeClasses)
+
+# Calculate "core recruits", i.e., recruits from the 3 top age classes (1.2, 1.3, 1.4)
+# First, calculate Spawners1, Spawners2 (min, nominal, and max),
+# weir removals, and terminal harvest by brood year.  
+coreRecruitsByBrood <- recruitsByRYxBY %>%
+  filter(!is.na(Spawners1)) %>%
+  filter(Age != "Age1.1" & Age != "Age1.5") %>%
+  group_by(Population, BroodYear) %>%
+  summarize(Spawners1 = sum(Spawners1ByRYxBY, na.rm = T),
+            Spawners2.min = sum(Spawners2.minByRYxBY, na.rm = T),
+            Spawners2.nom = sum(Spawners2.nomByRYxBY, na.rm = T),
+            Spawners2.max = sum(Spawners2.maxByRYxBY, na.rm = T),
+            WeirRemovals = sum(WeirRemovalsByRYxBY, na.rm = T),
+            TermHarvest = sum(TermHarvestByRYxBY, na.rm = T),
+            # Use these values to calculate CoreRecruits1 and CoreRecruits2 
+            # (min, nominal, and max)
+            CoreRecruits1 = round(Spawners1 + WeirRemovals),
+            CoreRecruits2.min = round(Spawners2.min + WeirRemovals + TermHarvest),
+            CoreRecruits2.nom = round(Spawners2.nom + WeirRemovals + TermHarvest),
+            CoreRecruits2.max = round(Spawners2.max + WeirRemovals + TermHarvest),
+            NumCoreAgeClasses = sum(CoreAgeClass)) %>%
+  filter(NumCoreAgeClasses == 3) %>%
+  select(Population, BroodYear, CoreRecruits1:NumCoreAgeClasses)
 
 # Combine the spawners (by return year) and recruits (by brood year) data into a
   # single dataframe and write to csv
 spawnersRecruits <- spawnersByRY %>%
-  left_join(recruitsByBrood, by = c("Population", "ReturnYear" = "BroodYear")) %>%
-  rename(Year = ReturnYear) %>%
+  rename(BroodYear = ReturnYear) %>%
+  left_join(recruitsByBrood, by = c("Population", "BroodYear")) %>%
+  left_join(coreRecruitsByBrood, by = c("Population", "BroodYear")) %>%
   # Trim years and populations outside the study and remove intermediate variables
-  filter(Year > 1979) %>%
+  filter(BroodYear > 1979) %>%
   filter(Population != "Funny" & Population != "Slikok" & Population != "Quartz" & 
            Population != "Crooked.Hatchery" & Population != "Ninilchik.Hatchery") %>%
+  
+  # Calculate 2 indices of brood year productivity:
+  # recruits [escapement + terminal harvest] / spawner (including top 5 age classes)
+  # and core recruits [escapement + terminal harvest] / spawner (including top 3 age classes).
+  # Both indices use the nominal estimate of aerial survey visibility
+  mutate(RecruitsPerSpawner = ifelse(Spawners2.nom > 0 ,
+                                          Recruits2.nom / Spawners2.nom, NA),
+         CoreRecruitsPerSpawner = ifelse(Spawners2.nom > 0,
+                                              CoreRecruits2.nom / Spawners2.nom, NA)
+  ) %>%
   write_csv("./data/SpawnersRecruits.csv")
   
 # Plot the results -----------------
 
-# Make a new versions of each variable "CB" for complete broods, listing values for incomplete 
-# broods as NA (but not removing those rows) so ggplot doesn't interpolate across missing years
-spawnersRecruitsCB <- spawnersRecruits %>%
-  mutate(Spawners1CB = ifelse(Top5AgeClasses, Spawners1, NA),
-       Spawners2CB.min = ifelse(Top5AgeClasses, Spawners2.min, NA),
-       Spawners2CB.nom = ifelse(Top5AgeClasses, Spawners2.nom, NA),
-       Spawners2CB.max = ifelse(Top5AgeClasses, Spawners2.max, NA),
-       Recruits1CB = ifelse(Top5AgeClasses, Recruits1, NA),
-       Recruits2CB.min = ifelse(Top5AgeClasses, Recruits2.min, NA),
-       Recruits2CB.nom = ifelse(Top5AgeClasses, Recruits2.nom, NA),
-       Recruits2CB.max = ifelse(Top5AgeClasses, Recruits2.max, NA),
-       RecruitsPerSpawner1CB = ifelse(Spawners1CB > 0 ,
-                                      Recruits1CB / Spawners1CB, NA),
-       RecruitsPerSpawner2CB.nom = ifelse(Spawners2CB.nom > 0 ,
-                                           Recruits2CB.nom / Spawners2CB.nom, NA)
-       )
-
 # Plot an index of brood year productivity (recruits [escapement + terminal harvest] /
 # spawner) for each population over time, using the nominal estimate for visibility of
 # spawners to aerial surveys [45%]). Plot years with complete broods only
-productivity.lineplot <- ggplot(data = spawnersRecruitsCB, 
-                              aes(x = Year, y = RecruitsPerSpawner2CB.nom,
+productivity <- ggplot(data = spawnersRecruits, 
+                              aes(x = BroodYear, y = RecruitsPerSpawner,
                                   color = Population)) +
   geom_line() +
   geom_hline(yintercept = 1) +
@@ -181,14 +197,26 @@ productivity.lineplot <- ggplot(data = spawnersRecruitsCB,
                      limits = c(1980, 2010)) +
   scale_y_continuous(name = "Productivity index\n(escapement + terminal harvest / spawner)", 
                      labels = scales::comma) 
-productivity.lineplot
+productivity
 ggsave("./figs/Brood year productivity index.png")
 
-# Same plot but excluding aerial survey data
-spawnersRecruitsCBNoAerial <- filter(spawnersRecruitsCB, 
+coreProductivity <- ggplot(data = spawnersRecruits, 
+                          aes(x = BroodYear, y = CoreRecruitsPerSpawner,
+                              color = Population)) +
+  geom_line() +
+  geom_hline(yintercept = 1) +
+  scale_x_continuous(name = "Brood Year", breaks = seq(1980, 2010, by = 10), 
+                     limits = c(1980, 2010)) +
+  scale_y_continuous(name = "Core-age productivity index\n(escapement + terminal harvest / spawner)", 
+                     labels = scales::comma) 
+coreProductivity
+ggsave("./figs/Core-age brood year productivity index.png")
+
+# Same plots but excluding aerial survey data
+spawnersRecruitsNoAerial <- filter(spawnersRecruits, 
                                      !str_detect(EscapementMethod, "aerial"))
-productivityNoAerial.lineplot <- ggplot(data = spawnersRecruitsCBNoAerial, 
-                                        aes(x = Year, y = RecruitsPerSpawner2CB.nom,
+productivityNoAerial <- ggplot(data = spawnersRecruitsNoAerial, 
+                                        aes(x = BroodYear, y = RecruitsPerSpawner,
                                             color = Population)) +
   geom_line() +
   geom_hline(yintercept = 1) +
@@ -196,5 +224,17 @@ productivityNoAerial.lineplot <- ggplot(data = spawnersRecruitsCBNoAerial,
                      limits = c(1980, 2010)) +
   scale_y_continuous(name = "Productivity index\n(escapement + terminal harvest / spawner)", 
                      labels = scales::comma) 
-productivityNoAerial.lineplot
-ggsave("./figs/Brood year productivity index_no aerial.png")
+productivityNoAerial
+ggsave("./figs/Brood year productivity index_no aerial surveys.png")
+
+coreProductivityNoAerial <- ggplot(data = spawnersRecruitsNoAerial, 
+                               aes(x = BroodYear, y = CoreRecruitsPerSpawner,
+                                   color = Population)) +
+  geom_line() +
+  geom_hline(yintercept = 1) +
+  scale_x_continuous(name = "Brood Year", breaks = seq(1980, 2010, by = 10), 
+                     limits = c(1980, 2010)) +
+  scale_y_continuous(name = "Core-age productivity index\n(escapement + terminal harvest / spawner)", 
+                     labels = scales::comma) 
+coreProductivityNoAerial
+ggsave("./figs/Core-age brood year productivity index_no aerial surveys.png")
--- figs/README.md
+++ figs/README.md
@@ -2,9 +2,13 @@
 
 ### Directory for figures produced by reconstructions and analyses
 
-Name            			         	    | Description
---------------------------------------------|---------------------------
-Brood year productivity index.png	 	    | Index of brood year productivity vs year for all
-									  		| study populations
-Brood year productivity index_no aerial.png | Same plot excluding aerial survey data
-HarvestByFishery.png						| Harvest of Chinook salmon by fishing sector
+Name            			         			   		| Description
+--------------------------------------------------------|---------------------------
+Brood year productivity index.png	 				    | Index of brood year productivity vs
+									  					| year for all study populations
+Brood year productivity index_no aerial surveys.png 	| Same plot excluding aerial survey data
+Core-age brood year productivity index.png	 			| Index of brood year productivity vs
+									  					| year for all study populations
+									  					| (3 core age classes only)
+Core-age brood year productivity index_no aerial surveys.png | Same plot excluding aerial survey data
+HarvestByFishery.png									| Harvest of Chinook salmon by fishing sector
