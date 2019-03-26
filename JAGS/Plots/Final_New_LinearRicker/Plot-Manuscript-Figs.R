09199a2030c6d72de380e874474dfebfd3e426bf
Curry Cunningham <cunninghamcurry@gmail.com>
2019-03-25 16:43:12 -0800

    Updated manuscript figures.

--- JAGS/Plots/Final_New_LinearRicker/Plot-Manuscript-Figs.R
+++ JAGS/Plots/Final_New_LinearRicker/Plot-Manuscript-Figs.R
@@ -14,6 +14,8 @@
 #==================================================================================================
 
 require(ggplot2)
+require(ggthemes)
+require(ggridges)
 require(R2jags)
 require(reshape2)
 require(mcmcplots)
@@ -137,13 +139,93 @@ dev.off()
 # Benefit - Shows consistence in other effects. 
 
 #Extract Effects
-eff.list.3 <- data.frame(out.3$BUGSoutput$sims.list$mu.coef)
-names(eff.list.3) <- 
-                 
-#Add Phase 3 Effects
+eff.df.3 <- data.frame(out.3$BUGSoutput$sims.list$mu.coef)
+names(eff.df.3) <- names.covars.3
+eff.list.3 <- eff.df.3 %>% melt()
+eff.list.3$Model <- "Model 1"
 
-#Add Phase 4 Effects.
+eff.df.4 <- data.frame(out.4$BUGSoutput$sims.list$mu.coef)
+names(eff.df.4) <- names.covars.4
+eff.list.4 <- eff.df.4 %>% melt()
+eff.list.4$Model <- "Model 2"
 
+#Combine
+eff.list.comb <- rbind(eff.list.3, eff.list.4)
+eff.list.comb$title <- "Group Mean Effect Across Stocks"
+
+#Prder Covariate factor
+eff.list.comb$variable <- factor(eff.list.comb$variable,
+                                   levels=c('wksGT13','wksGT15', 
+                                            'maxT_spawn','avgT_grow',
+                                            'maxP_spawn','avgP_grow',
+                                            'RB_spawn','RB_emerge',
+                                            'medianQ',
+                                            'breakup','NPGO'))
+
+
+
+
+#Plot It Out
+g <- ggplot(eff.list.comb, aes(value, group=Model, fill=Model)) +
+       theme_linedraw() +
+       scale_fill_gdocs() +
+       geom_vline(xintercept=0, col='red') +
+       geom_density(alpha=0.25) + 
+       facet_wrap(~variable) +
+       
+       # xlab('Covariate') +
+       xlab('Coefficient Value') +
+       ylab('Relative Probability Density') +
+       theme(legend.position='top',
+             axis.text.y=element_blank())
+ggsave(file.path(dir.figs,'2-Model Mu Posteriors.png'), plot=g,
+         height=6, width=7, dpi=500, units='in')
+
+#Necessary functions to calculate caterplots
+q.50 <- function(x) { return(quantile(x, probs=c(0.25,0.75))) }
+q.95 <- function(x) { return(quantile(x, probs=c(0.025,0.975))) }
+q_0.025 <- function(x) { return(quantile(x, probs=0.025)) }
+q_0.975 <- function(x) { return(quantile(x, probs=0.975)) }
+
+#Boxplots
+g <- ggplot(eff.list.comb, aes(x=variable, y=value, fill=Model)) +
+  theme_linedraw() +
+  scale_fill_gdocs() +
+  geom_hline(yintercept=0, col='red') +
+  # geom_violin(alpha=0.25) +
+  geom_boxplot(outlier.shape=NA, alpha=0.5) +
+  # geom_violin(alpha = 0.5, lwd=0.1, scale='width') +
+  
+  # stat_summary(fun.y="q.95", colour="black", geom="line", lwd=0.5) +
+  # stat_summary(fun.y="q.50", colour="black", geom="line", lwd=1.25) +
+  # stat_summary(fun.y="median", colour="black", size=1.75, geom="point", pch=21) +
+
+  # geom_density_ridges2() +
+
+  xlab('Covariate') +
+  ylab('Coefficient Value') +
+  theme(legend.position='top') +
+  coord_flip(ylim=c(-0.25,0.25)) +
+  scale_x_discrete(limits=rev(levels(eff.list.comb$variable)))+
+  facet_wrap(~title)
+  # coord_cartesian(xlim=c(-0.3,0.3))
+g
+ggsave(file.path(dir.figs,'2-Model Mu Boxplots.png'), plot=g,
+       height=5, width=5, dpi=500, units='in')
+
+#Violin plots
+g <- ggplot(eff.list.comb, aes(x=variable, y=value, fill=Model)) +
+  theme_linedraw() +
+  scale_fill_gdocs() +
+  geom_hline(yintercept=0, col='red') +
+  geom_violin(alpha=0.25) +
+  xlab('Covariate') +
+  ylab('Coefficient Value') +
+  theme(legend.position='top') +
+  coord_flip(ylim=c(-0.25,0.25)) +
+  facet_wrap(~title)
+# coord_cartesian(xlim=c(-0.3,0.3))
+g
 
 
 
